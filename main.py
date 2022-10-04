18804401dd0ec9ddbbf28d93194eeb1c06ccdeec
Princeroy1 <105480785+Princeroy1@users.noreply.github.com>
2022-10-04 04:35:59 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ s=soup.find_all('div',class_='stm_archive_product_inner_grid_content')
 # for title in s:
 s=soup.find_all('li',class_=['col-md-4' ,'col-sm-4' ,'col-xs-6'])
 p=soup.find_all('h5')
-print(s)
+print(s
+      
+      hbjm
     
     # print(title.text)
